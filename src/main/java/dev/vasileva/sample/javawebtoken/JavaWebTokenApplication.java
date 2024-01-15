5fa17c51600d8793baa9a46a01add780bf9a618a
katiavasileva <eka.vasileva@gmail.com>
2024-01-15 23:50:07 +0500

    Initial Commit

---
+++ src/main/java/dev/vasileva/sample/javawebtoken/JavaWebTokenApplication.java
@@ -0,0 +1,13 @@
+package dev.vasileva.sample.javawebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vasileva/sample/javawebtoken/JavaWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vasileva.sample.javawebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
